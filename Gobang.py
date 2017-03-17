2e7343b8db23f8b70e549906a22d8c72bc2ec97e
liulan19920228 <lanliu@umail.ucsb.edu>
2017-03-16 22:16:24 -0700

    Update Gobang.py

--- Gobang.py
+++ Gobang.py
@@ -1,26 +1,33 @@
 import re
+import sys
 #from random import randint
 from board import GameBoard
 from players import Human_Player, AI_Player
 import time
 
-#def set_play_order(players):
-#    whos_first = randint(1,2)
-#    # If number was 2, we want to reverse the player's order
-#    if (whos_first == 2):
-#        players.reverse()
-
 players = []
-size=11
-depth=2
+size = 11
+depth = 2
+Reverse = False
+for i in range(len(sys.argv)):
+    if sys.argv[i] == ‘-l’
+       Reverse = True
+    if sys.argv[i] == ‘-n’
+       size = int(sys.argv[i+1])
+end
+
 board = GameBoard(size)
-players.append(AI_Player("AI", board, 0, depth))
-players.append(Human_Player("Human", board, 1, depth))
+if not Reverse:
+    players.append(Human_Player("human", board, 0, depth))
+    players.append(AI_Player("COM", board, 1, depth))
+else:
+    players.append(Human_Player("COM", board, 0, depth))
+    players.append(AI_Player("human", board, 1, depth))
+        
 
 playing = True
 while playing:
-#    set_play_order(players)
-    print "\nMATCH: %s vs %s." % (players[0].get_name(), players[1].get_name())
+#    print "\nMATCH: %s vs %s." % (players[0].get_name(), players[1].get_name())
     print board
     moves = 0
     current_player_id = 0
@@ -28,11 +35,11 @@ while playing:
     while active_turn:
         moves += 1
         player = players[current_player_id]       
-        print "%s's turn: Move #%d..." % (player.get_name(), moves)
+#        print "%s's turn: Move #%d..." % (player.get_name(), moves)
         start_time=time.time()
         player.make_move()
         print board
-        print time.time()-start_time
+#        print time.time()-start_time
         if board.winner != None:
             active_turn = False
         else:
@@ -41,11 +48,16 @@ while playing:
             else:
                 current_player_id = 1
 
-    if (board.winner == 0 or 1):
-        print "The WINNER is %s!\n" % players[board.winner].get_name()
+    if (board.winner == 0 ):
+        print "Dark plyer %s wins!\n" % players[board.winner].get_name()
+    elif (board.winner == 1):
+        print "Light player %s wins!\n" % players[board.winner].get_name()
     else:
         print "It was a TIE game!\n"
 
     playing = False
+
+
+
 
 
